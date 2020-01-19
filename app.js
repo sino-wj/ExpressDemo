b52ff1cb927c7b500eecd560dd2c29babf4d7fd6
sino-wj <874015174@qq.com>
2020-01-19 17:23:54 +0800

    添加新接口

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/wj',function(req,res){
+  res.send("i have a apple");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
