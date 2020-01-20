59854ef7427eccff4f8755ad1e996450a623aadc
sino-wj <874015174@qq.com>
2020-01-20 15:58:21 +0800

    add aaa interface

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/aaa',function(req,res,next){
+  res.send("wo shi aaa");
+});
 module.exports = router;
