8a53522842d74eda17bf1eb6e80bca2b8e00e43b
sino-wj <874015174@qq.com>
2020-01-20 15:49:07 +0800

    安装依赖

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(multer());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -34,7 +36,13 @@ app.get('/api/getUsers',function(req,res){
     res.send(response);
 });
 
-
+app.post("/api/postUsers",function(req,res){
+    let response = {
+      "first_name":req.body.first_name,
+      "last_name":req.body.last_name
+    };
+    res.send(JSON.stringify(response));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
